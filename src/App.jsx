8c3585384cc6bacbc582e46e0130554c0cc46add
DmitriyShkredov <dmitriyshkredovdev@gmail.com>
2023-08-21 14:48:29 +0300

    feat: add webcam

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,48 @@
-import logo from "./logo.svg";
+import React, { useState, useRef } from "react";
+import Webcam from "react-webcam";
 import "./App.css";
 
 function App() {
+  const [photo, setPhoto] = useState(null);
+  const [size, setSize] = useState({ width: 0, height: 0 });
+  const videoRef = useRef(null);
+
+  const onMakePhoto = () => {
+    const screenshot = videoRef.current.getScreenshot();
+    setPhoto(screenshot);
+  };
+
+  const onLoadPhoto = (e) => {
+    setSize({ width: e.target.naturalWidth, height: e.target.naturalHeight });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Типичный веб разработчик</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Получаем высокое разрешение видео и фото из камеры
-        </a>
-      </header>
-    </div>
+    <section className="section">
+      <Webcam
+        ref={videoRef}
+        audio={false}
+        screenshotQuality={1}
+        screenshotFormat="image/jpeg"
+        forceScreenshotSourceSize={true}
+        videoConstraints={{
+          facingMode: "user",
+          width: {
+            min: 640,
+            max: 2560,
+          },
+          height: {
+            min: 480,
+            max: 1440,
+          },
+        }}
+      />
+      <button onClick={onMakePhoto}>Photo</button>
+      <img src={photo} onLoad={onLoadPhoto} alt="" />
+      <p>
+        Width: {size.width} <br />
+        Height: {size.height}
+      </p>
+    </section>
   );
 }
 
