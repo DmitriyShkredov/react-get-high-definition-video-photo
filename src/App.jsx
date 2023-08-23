2ccabf9202b271af42c6fc674dd474d315049f6c
DmitriyShkredov <dmitriyshkredovdev@gmail.com>
2023-08-23 17:26:28 +0300

    refactoring: refactoring App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,47 +3,64 @@ import Webcam from "react-webcam";
 import "./App.css";
 
 function App() {
+  const [cameraOn, setCameraOn] = useState(false);
   const [photo, setPhoto] = useState(null);
-  const [size, setSize] = useState({ width: 0, height: 0 });
+  const [resolution, setResolution] = useState(null);
   const videoRef = useRef(null);
 
   const onMakePhoto = () => {
-    const screenshot = videoRef.current.getScreenshot();
-    setPhoto(screenshot);
+    if (videoRef.current) setPhoto(videoRef.current.getScreenshot());
   };
 
   const onLoadPhoto = (e) => {
-    setSize({ width: e.target.naturalWidth, height: e.target.naturalHeight });
+    setResolution({
+      width: e.target.naturalWidth,
+      height: e.target.naturalHeight,
+    });
   };
 
   return (
     <section className="section">
-      <Webcam
-        ref={videoRef}
-        audio={false}
-        screenshotQuality={1}
-        screenshotFormat="image/jpeg"
-        forceScreenshotSourceSize={true}
-        videoConstraints={{
-          facingMode: "user",
-          width: {
-            min: 640,
-            ideal: 2560,
-            max: 2560,
-          },
-          height: {
-            min: 480,
-            ideal: 1440,
-            max: 1440,
-          },
-        }}
-      />
-      <button onClick={onMakePhoto}>Photo</button>
-      <img src={photo} onLoad={onLoadPhoto} alt="" />
-      <p>
-        Width: {size.width} <br />
-        Height: {size.height}
-      </p>
+      {cameraOn && (
+        <Webcam
+          ref={videoRef}
+          audio={false}
+          screenshotQuality={1}
+          screenshotFormat="image/jpeg"
+          forceScreenshotSourceSize={true}
+          videoConstraints={{
+            facingMode: "user",
+            width: {
+              min: 640,
+              max: 2560,
+              ideal: 2560,
+            },
+            height: {
+              min: 480,
+              max: 1440,
+              ideal: 1440,
+            },
+          }}
+        />
+      )}
+
+      <button className="on-off-button" onClick={() => setCameraOn(!cameraOn)}>
+        On /Off
+      </button>
+
+      <button className="photo-button" onClick={onMakePhoto}>
+        Photo
+      </button>
+
+      <img src={photo} alt="" className="photo" onLoad={onLoadPhoto} />
+
+      {resolution && (
+        <span className="resolution">
+          Width: {resolution.width}
+          <br />
+          Height: {resolution.height}
+        </span>
+      )}
     </section>
   );
 }
